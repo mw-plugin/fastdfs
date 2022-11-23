04077d53e40f688a4a8cf9e0908daa0e2dfc7c70
midoks <midoks@163.com>
2022-11-23 12:45:44 +0800

    init

---
+++ versions/6.09/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sysName=`uname`
+
+install_tmp=${rootPath}/tmp/mw_install.pl
+
+bash ${rootPath}/scripts/getos.sh
+OSNAME=`cat ${rootPath}/data/osname.pl`
+OSNAME_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
+
+
+VERSION=6.09
+Install_App()
+{
+
+	echo '正在安装脚本文件...' > $install_tmp
+	mkdir -p $serverPath/fastdfs
+	APP_DIR=${serverPath}/source/fastdfs
+
+	mkdir -p $APP_DIR
+	
+	if [ ! -f ${APP_DIR}/fastdfs-${VERSION}.tar.gz ];then
+		if [ $sysName == 'Darwin' ]; then
+			wget -O ${APP_DIR}/fastdfs-${VERSION}.tar.gz https://github.com/happyfish100/fastdfs/archive/refs/tags/V${VERSION}.tar.gz
+		else
+			curl -sSLo ${APP_DIR}/fastdfs-${VERSION}.tar.gz https://github.com/happyfish100/fastdfs/archive/refs/tags/V${VERSION}.tar.gz
+		fi
+	fi
+
+	if [ ! -f ${APP_DIR}/fastdfs-${VERSION}.tar.gz ];then
+		curl -sSLo ${APP_DIR}/fastdfs-${VERSION}.tar.gz https://gitee.com/fastdfs100/fastdfs/archive/refs/tags/V${VERSION}.tar.gz
+	fi
+
+	cd ${APP_DIR} && tar -zxvf fastdfs-${VERSION}.tar.gz
+
+
+	echo $MIN_VERSION > $serverPath/fastdfs/version.pl
+	echo 'Install_HA' > $install_tmp
+}
+
+Uninstall_App()
+{
+	# if [ -f $serverPath/fastdfs/initd/fastdfs ];then
+	# 	$serverPath/fastdfs/initd/fastdfs stop
+	# fi
+
+	rm -rf $serverPath/fastdfs
+	echo "Uninstall_HA" > $install_tmp
+}
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_App
+else
+	Uninstall_App
+fi
